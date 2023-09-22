4dd4731eb0a78e7767162e09c1c261ab14da67a2
Farkas Balázs <farkasb22@ADY-NAGYATAD.SULINET.HU>
2023-09-22 13:16:50 +0200

    maiak

---
+++ py0922.py
@@ -0,0 +1,25 @@
+#Irassátok ki az 50 első nemnegatív számot!
+
+for i in range(0,50):
+    print(i, end="; ")
+else:
+    print("\n Kész!")
+
+#Irassátok ki a kétjegyű számokat!
+
+for a in range(10,100):
+    print(a, end="; ")
+else:
+    print("\n Kész!")
+
+#Irassátok ki a 100-nál kisebb 7-tel osztható számokat!
+
+for b in range(7, 101, 7):
+    print(b)
+else:
+    print("\n Kész!")
+#Irassátok ki az 500 és 2000 között azokat a számokat ahol a tízesek helyén 0 van.
+
+#Irassátok ki azokat a négyzetszámokat, amelyek 3 számjegyűek
+
+#Számold meg, mennyi 3 jegyű négyzetszám van
