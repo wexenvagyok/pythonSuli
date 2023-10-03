2a484d72f8861019c170e12c3fa6778f8c6e8438
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2023-10-03 12:00:36 +0200

    Add files via upload

---
+++ 0929.py
@@ -0,0 +1,19 @@
+import math
+
+a=int(input("a: "))
+b=int(input("b: "))
+
+c=math.sqrt(a*a+b*b)
+
+print("c:" ,c)
+
+if (c-c//1==0):
+    print("Egész")
+else:
+    print("Nem egész")
+
+for a in range(1,100):
+    for b in range(a,10):
+        c=math.sqrt(a*a+b*b)
+        if (c-c//1==0):
+            print(a,b,c)
